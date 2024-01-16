aeee625e0111d184c3339b873d967fd1e992914d
Linda Sherman <eliseest1900@gmail.com>
2024-01-16 11:39:34 -0500

    Adding files

---
+++ stories.py
@@ -0,0 +1,45 @@
+"""Madlibs Stories."""
+
+
+class Story:
+    """Madlibs story.
+
+    To  make a story, pass a list of prompts, and the text
+    of the template.
+
+        >>> s = Story(["noun", "verb"],
+        ...     "I love to {verb} a good {noun}.")
+
+    To generate text from a story, pass in a dictionary-like thing
+    of {prompt: answer, promp:answer):
+
+        >>> ans = {"verb": "eat", "noun": "mango"}
+        >>> s.generate(ans)
+        'I love to eat a good mango.'
+    """
+
+    def __init__(self, words, text):
+        """Create story with words and template text."""
+
+        self.prompts = words
+        self.template = text
+
+    def generate(self, answers):
+        """Substitute answers into text."""
+
+        text = self.template
+
+        for (key, val) in answers.items():
+            text = text.replace("{" + key + "}", val)
+
+        return text
+
+
+# Here's a story to get you started
+
+
+story = Story(
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """Being stuck at home during a/an {noun} storm is the WORST! My {adjective} brother has been banging on his {plural_noun} all day. 
+    It's driving me totally crazy. I prefer being at {place}, Dad has {plural_noun} stuffed in his bed and my dog does not stop {verb}."""
+)
